0aabe9b0280f92ce4d48054cdcf0894980c1402f
Ayormitan <kolawoleabaz97@gmail.com>
2023-12-20 08:31:13 -0800

    Function creates an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""
-A class to define a square
-"""
+
+"""A class to define a square"""
 
 class Square:
-    """
-    this class represents the square.
-    """
+    """this class represents the square."""
     pass
