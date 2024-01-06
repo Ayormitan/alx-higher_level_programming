f516e209d8932e58f81ad1943cf15a2ffa18cd0b
Ayormitan <kolawoleabaz97@gmail.com>
2024-01-06 10:03:42 -0800

    Class function with public instance

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,35 +9,35 @@ class Rectangle:
             width
             height
         """
-        self._width = width
-        self._height = height
+        self.__width = width
+        self.__height = height
     @property
     def width(self):
         """ Width getter"""
-        return self._width
-    @property
-    def height(self):
-        """heigth getter"""
-        return self._height
+        return self.__width
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
+    @property
+    def height(self):
+        """heigth getter"""
+        return self.__height
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._heigth = value
+        self.__heigth = value
     def area(self):
         """ area of the rectangle"""
-        return self.width * self.height
+        return self.__width * self.__height
     def perimeter(self):
         """"perimeter of the rectangle"""
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * self.width + self.height
+        return 2 * self.__width + self.__height
