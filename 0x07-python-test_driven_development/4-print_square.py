b1653ef5164806a66173bcd6336b79fa32e32d89
Ayormitan <kolawoleabaz97@gmail.com>
2024-01-11 11:22:13 -0800

    Function prints char #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,6 +12,5 @@ def print_square(size):
     if not isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print()
+           [print("#", end="") for j in range(size)]
+        print("")
