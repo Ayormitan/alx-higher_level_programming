29aba0ed472705571152db7aef4b84c76cc3da1b
Ayormitan <kolawoleabaz97@gmail.com>
2024-01-11 12:30:21 -0800

    function prints text and two line after a char

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,11 +5,18 @@ def text_indentation(text):
     args:
         test: character to test
     """
-    char_to_check = ['.', '?', ':']
-    for char in text:
-        print(char, end="")
-        if char in char_to_check:
-            print("\n" * 2, end="")
-    print()
-    if not isinstance(text, string):
+    if not isinstance(text, str):
         raise TypeError("textmust be a string")
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
