57169eeb37c19d73a534296524cd2f1734a381cc
Ayormitan <kolawoleabaz97@gmail.com>
2024-01-12 08:41:12 -0800

    Return object/instance type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """
+    Checks if the object is exactly an instance of the specified class.
+            
+    Args:
+    - obj: An object to check.
+    - cls: A class or tuple of classes to compare against.
+                            
+    Returns:
+     - True if obj is exactly an instance of cls; otherwise, False.
+    """
+    return (type(obj)) == a_class
