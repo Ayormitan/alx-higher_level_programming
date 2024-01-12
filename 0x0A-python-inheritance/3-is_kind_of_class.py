d9a09a47b1488cbc84e7c0387666182836581229
Ayormitan <kolawoleabaz97@gmail.com>
2024-01-12 09:13:19 -0800

    Usage of isinstance in class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/ptyhon3
 """ Function defines class and instance"""
 def is_kind_of_class(obj, a_class):
-    """ 
-    Function will check for type of class using isinstance
+    """ Function will check for type of class using isinstance
     args:
         obj: Object to check
         a_class: class type
+        Returns: True If object is an instance or it is inherited else false
     """
     if isinstance(obj, a_class):
         return True
