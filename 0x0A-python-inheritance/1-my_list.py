1a5622c29373adea6375df2b4da3ed0fb1f1123b
Ayormitan <kolawoleabaz97@gmail.com>
2024-01-12 07:53:00 -0800

    Claa returns and sort list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Define inherited list"""
-class Mylist(list):
+class MyList(list):
     """ Print sorted list for already built in list"""
     def print_sorted(self):
         print(sorted(self))
