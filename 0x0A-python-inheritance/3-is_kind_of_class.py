4d8e6de359882a1e02eeafc41bafc2f1d6eea6c0
Ayormitan <kolawoleabaz97@gmail.com>
2024-01-12 09:22:55 -0800

    Test function for def is_kind_of_class(obj, a_class):

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,10 +2,11 @@
 """ Function defines class and instance"""
 def is_kind_of_class(obj, a_class):
     """ Function will check for type of class using isinstance
-    args:
-        obj: Object to check
-        a_class: class type
-        Returns: True If object is an instance or it is inherited else false
+    Args:
+        obj (any): Object to check
+        a_class (type): class type
+    Returns:
+            true If object is an instance or it is inherited else false
     """
     if isinstance (obj, a_class):
         return True
