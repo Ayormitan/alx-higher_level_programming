5e6da6479b5dc752fae12d999d2ae2c5f21c8038
Ayormitan <kolawoleabaz97@gmail.com>
2024-01-19 01:35:37 -0800

    Crested class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""A class defined in base"""
 class Base:
+    """
+    private class attribute
+    """
     __nb_objects = 0  # private class attribute
 
     def __init__(self, id=None):
+        """ initializes attribute"""
         if id is not None:
             self.id = id  # assign id if provided
         else:
