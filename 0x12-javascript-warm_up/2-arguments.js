7c9d0cb66b4ad1a343fbe730aa9b4c12b1b81428
Ayormitan <kolawoleabaz97@gmail.com>
2024-03-16 08:51:26 -0700

    Using process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 /**
  * Using process arg
+ * fiels
  */
 const args = process.argv;
-	if (args.length <= 2)
-	{
-		console.log('No argument');
-	}
-	else
-	{
-		console.log('Argument found');
-	}
+
+if (args.length <= 3)
+{
+	console.log('No argument');
+}
+else
+{
+	console.log('Argument found');
+}
