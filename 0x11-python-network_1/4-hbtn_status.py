de04c4b7e07557b03672f6e03f8f00ee2df7c9c0
Ayormitan <kolawoleabaz97@gmail.com>
2024-03-30 12:18:12 -0700

    Using requests to fecth url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,5 +3,5 @@ import requests
 if __name__ == "__main__":
     html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type:", type(html.text))
+    print("\t- type: {}".format(type(html.text)))
     print("\t- content: {}".format(html.text))
