ea7d75f6e972995f0aaee3bfbc7b448d4b9053fc
Ayormitan <kolawoleabaz97@gmail.com>
2024-04-08 05:54:51 -0700

    Print recursively

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,8 +2,8 @@
 function factorial (n) {
   if (n < 0) {
     return (-1);
-   }
-  if (n === 0 || isNan(n)) {
+  }
+  if (n === 0 || isNaN(n)) {
     return (1);
   }
   return (n * factorial(n - 1));
